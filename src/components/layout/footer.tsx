4f0732758a073a58f06c3290e7d152407097149c
Matheus Padilha <padilha.matheus@hotmail.com>
2026-07-29 21:47:13 -0300

    Adiciona logo Kaiser no footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -47,10 +47,12 @@ export function Footer() {
 				<span>
 					Desenvolvido com ❤️ por{" "}
 					<a
+						className="event-footer__credit-link"
 						href="https://kaisertec.com.br?utm_source=epje&utm_medium=footer&utm_campaign=epje_pg"
 						target="_blank"
 						rel="noopener"
 					>
+						<img src="/kaiserPreto.png" alt="" aria-hidden="true" />
 						Kaiser Tech
 					</a>
 				</span>
